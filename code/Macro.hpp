2d3ca539745cbf2f72d785af82749d8e4ac7f119
10chen01 <109595373+10chen01@users.noreply.github.com>
2022-11-26 23:51:21 +0000

    new: ASNI ctrl code

--- code/Macro.hpp
+++ code/Macro.hpp
@@ -2,21 +2,48 @@
 
 #define count_for(cnt) for(int i=1;i<=cnt;i++)
 #define pr_cnt_char(cnt, ch) count_for(cnt){printf(ch);}
+#define pr_line(ch) pr_cnt_char(20)
 
+// ASNI normal text
 #define reset "\033[0m"
 
-#define bold_beg "\033[1m"
-#define light_beg "\033[2m"
-#define italic_beg "\033[4m"
-#define u_shad_beg "\033[5m"
+// ASNI style
+#define bold(str) "\033[1m"#str
+#define light(str) "\033[2m"#str
+#define italic(str) "\033[4m"#str
+#define u_shad(str) "\033[5m"#str
 
-#define _default 39
-#define _black 30
-#define _red 31
-#define _green 32
-#define _yellow 33
-#define _blue 34
-#define _purple 35
-#define _magenta _purple
-#define _cyan 36
-#define _white 37
+// ASNI color normal
+#define __default 39
+#define __black 30
+#define __red 31
+#define __green 32
+#define __yellow 33
+#define __blue 34
+#define __purple 35
+#define __magenta _purple
+#define __cyan 36
+#define __white 37
+
+// ASNI color style
+#define fg_default(str) "\033[39"#str
+#define fg_black(str) "\033[30"#str
+#define fg_red(str) "\033[31"#str
+#define fg_green(str) "\033[32"#str
+#define fg_yellow(str) "\033[33"#str
+#define fg_blue(str) "\033[34"#str
+#define fg_purple(str) "\033[35"#str
+#define fg_magenta(str) _fg_purple(str)
+#define fg_cyan(str) "\033[36"#str
+#define fg_white(str) "\033[37"#str
+
+#define bg_default(str) "\033[49"#str
+#define bg_black(str) "\033[40"#str
+#define bg_red(str) "\033[41"#str
+#define bg_green(str) "\033[42"#str
+#define bg_yellow(str) "\033[43"#str
+#define bg_blue(str) "\033[44"#str
+#define bg_purple(str) "\033[45"#str
+#define bg_magenta(str) _bg_purple(str)
+#define bg_cyan(str) "\033[46"#str
+#define bg_white(str) "\033[47"#str
