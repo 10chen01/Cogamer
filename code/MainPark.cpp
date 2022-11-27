2ab1ce67493efbbc12b6ab355926b92456c92415
10chen01 <109595373+10chen01@users.noreply.github.com>
2022-11-27 00:55:36 +0000

    xxx

--- code/MainPark.cpp
+++ code/MainPark.cpp
@@ -3,8 +3,11 @@
 #include "HeaderInfo.hpp"
 #include "Macro.hpp"
 
+
+
 int main(){
     pr_cnt_char(20, "=");
+    // Title
     printf("\n" bold_fg_yellow(Welcome to Cogamer!) reset "\n");
     
     pr_cnt_char(20, "=");
